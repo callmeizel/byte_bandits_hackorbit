1751a2bc2ada6193b9b7352124e5c5cff21f2115
Gaurav <gauravthapa2727@gmail.com>
2025-07-09 08:16:49 +0530

    added aiBot prototype

---
+++ aiBot-code/projectpal.py
@@ -0,0 +1,39 @@
+from openai import OpenAI, AuthenticationError, APIError
+
+# key - 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
+
+client = OpenAI(
+        api_key = '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',
+        base_url = "https://openrouter.ai/api/v1"
+)
+
+
+def project_pal():
+    
+    prompt = input("Ask anything : ")
+    
+    try:
+        response = client.chat.completions.create(
+            model = "deepseek/deepseek-r1-0528:free",
+            messages = [
+                {
+                    'role':'user',
+                    'content':prompt
+                }
+            ],
+            temperature = 0,
+            max_tokens = 5000,
+            stream = True
+        )
+        for chunk in response:
+            if chunk.choices[0].delta.content:
+                print(chunk.choices[0].delta.content,end='',flush=True)
+                
+    except AuthenticationError:
+        print('Authentication error / API key not working...')
+    except APIError as apierr:
+        print(f"API error : {apierr}")
+    except Exception as exp:
+        print(f"Error : {exp}")
+
+project_pal()
---
+++ aiBot-code/techbuddy.py
@@ -0,0 +1,43 @@
+from openai import OpenAI, AuthenticationError, APIError
+
+# key - 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
+
+client = OpenAI(
+        api_key = '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',
+        base_url = "https://openrouter.ai/api/v1"
+)
+
+
+def techbuddy():
+    
+    prompt = input("Ask anything : ")
+    
+    try:
+        response = client.chat.completions.create(
+            model = "deepseek/deepseek-r1-0528:free",
+            messages = [
+                {
+                  'role':'system',
+                  'content':' As an expert in field of Computer Science. Answer questions accurately and with beginner friendly language'  
+                },
+                {
+                    'role':'user',
+                    'content':prompt
+                }
+            ],
+            temperature = 0,
+            max_tokens = 5000,
+            stream = True
+        )
+        for chunk in response:
+            if chunk.choices[0].delta.content:
+                print(chunk.choices[0].delta.content,end='',flush=True)
+                
+    except AuthenticationError:
+        print('Authentication error / API key not working...')
+    except APIError as apierr:
+        print(f"API error : {apierr}")
+    except Exception as exp:
+        print(f"Error : {exp}")
+
+techbuddy()
